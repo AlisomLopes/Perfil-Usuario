e691063d36a8cc539ed726945614ac76f7b51e9d
AlisomLopes <alisomlopesps@gmail.com>
2024-04-10 16:57:52 -0300

    interactive profile

--- main.js
+++ main.js
@@ -1,7 +1,23 @@
-function editingProfile()
-{   
-    if(!document.getElementById('input-name-profile').value || !document.getElementById('input-biography').value)
+function showDisplay()
+{
+    document.getElementById('button-edit').addEventListener('click', function()
     {
+        this.style.display = "none";
+        document.getElementById('form').style.display = 'flex';
+    })
+}
+
+function hiddenDisplay(){
+
+    document.getElementById('form').style.display = "none";
+    document.getElementById('button-edit').style.display = 'flex';
+
+}
+
+function editingProfile() //função do botão submit
+{   
+    if(!document.getElementById('input-name-profile').value || !document.getElementById('input-biography').value) //verificação se os input's profile name e biography estão vazios
+    {//se estiverem(verdadeiro), será exibido um alerta e o botão submite não irá funcionar
         alert('To edit your profile, you must fill in all fields!');
         document.getElementById('save-button').addEventListener('submit', function(event)
         {   
@@ -9,9 +25,18 @@ function editingProfile()
         })
     }
     else
-    {
-        document.getElementById('name-profile').innerText = document.getElementById('input-name-profile').value;
+    { //senão, tudo ocorrerá normalmente
+        let facebook = document.getElementById('input-facebook').value;//variavel recebe o valor do input url facebook
+        let instagram = document.getElementById('input-instagram').value;//variavel recebe o valor do input url instagram
+        let twitter_x = document.getElementById('input-twitter/x').value;//variavel recebe o valor do input url twitter
 
-        document.getElementById('biography-profile').innerText = document.getElementById('input-biography').value;
+        document.getElementById('name-profile').innerText = document.getElementById('input-name-profile').value;//o valor do <p> com 'id name-profile' será substituido pelo valor que o usuário inseriu no input-name-profile
+
+        document.getElementById('biography-profile').innerText = document.getElementById('input-biography').value;//o valor do <p> com 'id biography-profile' será substituido pelo valor que o usuário inseriu no input-biography
+
+        document.getElementById('facebook').innerHTML = `<a href="${facebook}" class="links" target="_blank" id="facebook">facebook</a>`
+        document.getElementById('instagram').innerHTML = `<a href="${instagram}" class="links" target="_blank" id="instagram">instagram</a>`
+        document.getElementById('twitter/x').innerHTML = `<a href="${twitter_x}" class="links" target="_blank" id="twitter_x">twitter_x</a>`//os valores do <a href> serão substituidos pelo valor das variaveis locais no começo do bloco, que são, por sua vez, os valores inseridos pelo usuário nas 'inputs type "url"'.
+        hiddenDisplay()
     }
 }
