044511f4560cf7dde6b046f0e229fa0865dd5d35
AlisomLopes <alisomlopesps@gmail.com>
2024-04-11 13:19:54 -0300

    versão de entrega da atividade

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Perfil-Usuario
+Página criada para a atividade final do curso de Front-End do SENAI
+
+Segue o enunciado:
+
+"Você foi contratado por uma empresa chamada "TechSolutions" para criar uma página de perfil de usuário (pets) de forma personalizável. A empresa deseja que os usuários possam criar seus perfis, adicionando informações como nome, foto (opcional na nova inclusão), biografia e links para alavancar o processo de doação nas por meio das redes sociais.
+Para atender a esses requisitos, você precisa utilizar HTML para estruturar o conteúdo da página, CSS para estilizar os elementos e tornar o design atraente, e JavaScript para adicionar funcionalidades interativas, como validar os campos do formulário e exibir uma os dados enviados do perfil.
+Após concluir o desenvolvimento da página, você deve hospedar o projeto no GitHub. Isso envolve criar um repositório no GitHub, fazer o upload dos arquivos do projeto, e fornecer uma descrição clara e concisa do que foi desenvolvido e como testar a aplicação."
--- main.js
+++ main.js
@@ -4,20 +4,21 @@ function showDisplay()
     {
         this.style.display = "none";
         document.getElementById('form').style.display = 'flex';
+        document.getElementById('name-bio-links').style.display = 'none';
     })
 }
 
-function hiddenDisplay(){
-
+function hiddenDisplay()
+{
     document.getElementById('form').style.display = "none";
     document.getElementById('button-edit').style.display = 'flex';
-
+    document.getElementById('name-bio-links').style.display = 'flex';
 }
 
-function editingProfile() //função do botão submit
+function editingProfile()
 {   
-    if(!document.getElementById('input-name-profile').value || !document.getElementById('input-biography').value) //verificação se os input's profile name e biography estão vazios
-    {//se estiverem(verdadeiro), será exibido um alerta e o botão submite não irá funcionar
+    if(!document.getElementById('input-name-profile').value || !document.getElementById('input-biography').value)
+    {
         alert('To edit your profile, you must fill in all fields!');
         document.getElementById('save-button').addEventListener('submit', function(event)
         {   
@@ -25,18 +26,19 @@ function editingProfile() //função do botão submit
         })
     }
     else
-    { //senão, tudo ocorrerá normalmente
-        let facebook = document.getElementById('input-facebook').value;//variavel recebe o valor do input url facebook
-        let instagram = document.getElementById('input-instagram').value;//variavel recebe o valor do input url instagram
-        let twitter_x = document.getElementById('input-twitter/x').value;//variavel recebe o valor do input url twitter
+    {
+        let facebook = document.getElementById('input-facebook').value;
+        let instagram = document.getElementById('input-instagram').value;
+        let twitter_x = document.getElementById('input-twitter/x').value;
 
-        document.getElementById('name-profile').innerText = document.getElementById('input-name-profile').value;//o valor do <p> com 'id name-profile' será substituido pelo valor que o usuário inseriu no input-name-profile
+        document.getElementById('name-profile').innerText = document.getElementById('input-name-profile').value;
 
-        document.getElementById('biography-profile').innerText = document.getElementById('input-biography').value;//o valor do <p> com 'id biography-profile' será substituido pelo valor que o usuário inseriu no input-biography
+        document.getElementById('biography-profile').innerText = document.getElementById('input-biography').value;
 
         document.getElementById('facebook').innerHTML = `<a href="${facebook}" class="links" target="_blank" id="facebook">facebook</a>`
         document.getElementById('instagram').innerHTML = `<a href="${instagram}" class="links" target="_blank" id="instagram">instagram</a>`
-        document.getElementById('twitter/x').innerHTML = `<a href="${twitter_x}" class="links" target="_blank" id="twitter_x">twitter_x</a>`//os valores do <a href> serão substituidos pelo valor das variaveis locais no começo do bloco, que são, por sua vez, os valores inseridos pelo usuário nas 'inputs type "url"'.
+        document.getElementById('twitter/x').innerHTML = `<a href="${twitter_x}" class="links" target="_blank" id="twitter_x">twitter_x</a>`
+        
         hiddenDisplay()
     }
 }
